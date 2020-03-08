6b70a341c1f3f079f93499d3ccf01c6424dee13a
Godson <Godson777@users.noreply.github.com>
2020-03-08 05:05:39 -0400

    I swear I'm not retarded pt 2

--- KekBot/CommandExtensions.cs
+++ KekBot/CommandExtensions.cs
@@ -1,12 +1,16 @@
 ﻿using System.Linq;
 using Cmd = DSharpPlus.CommandsNext.Command;
+using Arg = DSharpPlus.CommandsNext.CommandArgument;
 using KekBot.Attributes;
+using DSharpPlus.CommandsNext;
 
 namespace KekBot {
     public static class CommandExtensions {
 
         public static Category GetCategory(this Cmd command) =>
             command?.CustomAttributes.OfType<CategoryAttribute>().FirstOrDefault()?.Category ?? Category.Uncategorized;
+
+        public static bool IsCustomRequired(this Arg argument) => argument?.CustomAttributes.OfType<RequiredAttribute>().Any() ?? false;
 
     }
 }
