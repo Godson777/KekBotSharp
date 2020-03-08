f552419732bb2537a0b6e575a9404587a08137df
Godson <Godson777@users.noreply.github.com>
2020-03-08 05:04:35 -0400

    Should we keep `avatar` in KekBot 2?

--- KekBot/Command/Commands/FunCommands.cs
+++ KekBot/Command/Commands/FunCommands.cs
@@ -8,12 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace KekBot.Command.Commands {
-    class FunCommands : BaseCommandModule {
+    public class FunCommands : BaseCommandModule {
+
+        private static readonly string[] EightBall = { "It is certain.", "It is decidedly so.", "Yes, definitely.", "You may rely on it.",
+        "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.",
+        "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.",
+        "Outlook not so good.", "Very doubtful." };
+        private static Random random = new Random();
+
         [Command("8ball"), Description("Ask the magic 8-ball a question!"), Category(Category.Fun)]
-        public async Task eightball(CommandContext ctx, [RemainingText, Description("The question to ask to the magic 8-ball.")] string question) {
-            if (question == "") {
-                
+        public async Task EightBallCommand(CommandContext ctx, [RemainingText, Description("The question to ask to the magic 8-ball.")] string question) {
+            CustomEmote emote = await CustomEmote.Get();
+            if (question == null) {
+                await ctx.RespondAsync($"{emote.Think} I asked: Did {ctx.User.Username} give you a question?\n\n🎱 8-Ball's response: No, they didn't.");
+            } else {
+                await ctx.RespondAsync($"{emote.Think} You asked: {question}\n\n🎱 8-Ball's response: {EightBall[random.Next(EightBall.Length)]}");
             }
+        }
+
+        [Command("avatar"), Description("Sends a larger version of the specified user's avatar.")]
+        [Aliases("ava"), Category(Category.Fun), Priority(0)]
+        public async Task AvatarCommand(CommandContext ctx, [Description("The user to pull the avatar from."), Required] DiscordMember? user = null) {
+            if (user == null) await ctx.RespondAsync("peepee");
+            else await ctx.RespondAsync(user.AvatarUrl);
         }
 
     }
