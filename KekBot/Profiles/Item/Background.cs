8ed61ae69030b359bb5607fc00394a0b6acc4ca0
Godson <Godson777@users.noreply.github.com>
2020-03-09 23:58:00 -0400

    Profiles! WIP.

---
+++ KekBot/Profiles/Item/Background.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace KekBot.Profiles.Item {
+    public class Background : Item {
+        public override Stream DrawItem() {
+            return new FileStream($"../../../../resources/profile/background/{File}", FileMode.Open);
+        }
+
+        
+    }
+}
---
+++ KekBot/Profiles/Item/ItemRegistry.cs
@@ -0,0 +1,14 @@
+﻿using KekBot.Profiles.Item;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KekBot.Profiles.Item {
+    public class ItemRegistry {
+        Dictionary<string, Item> registeredItems;
+
+        private ItemRegistry instance;
+
+        public ItemRegistry Get => instance ??= new ItemRegistry();
+    }
+}
