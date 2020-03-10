5d99fcb77cf24ef73f8849231cf3678ae6b0dc34
Godson <Godson777@users.noreply.github.com>
2020-03-09 23:22:38 -0400

    coinflip!

--- KekBot/Command/Commands/FunCommands.cs
+++ KekBot/Command/Commands/FunCommands.cs
@@ -14,7 +14,7 @@ namespace KekBot.Command.Commands {
         "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.",
         "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.",
         "Outlook not so good.", "Very doubtful." };
-        private static Random random = new Random();
+        private static Random Random = new Random();
 
         [Command("8ball"), Description("Ask the magic 8-ball a question!"), Category(Category.Fun)]
         public async Task EightBallCommand(CommandContext ctx, [RemainingText, Description("The question to ask to the magic 8-ball.")] string question) {
@@ -22,7 +22,7 @@ namespace KekBot.Command.Commands {
             if (question == null) {
                 await ctx.RespondAsync($"{emote.Think} I asked: Did {ctx.User.Username} give you a question?\n\n🎱 8-Ball's response: No, they didn't.");
             } else {
-                await ctx.RespondAsync($"{emote.Think} You asked: {question}\n\n🎱 8-Ball's response: {EightBall[random.Next(EightBall.Length)]}");
+                await ctx.RespondAsync($"{emote.Think} You asked: {question}\n\n🎱 8-Ball's response: {EightBall[Random.Next(EightBall.Length)]}");
             }
         }
 
@@ -32,6 +32,13 @@ namespace KekBot.Command.Commands {
             if (user == null) await ctx.RespondAsync("peepee");
             else await ctx.RespondAsync(user.AvatarUrl);
         }
+
+        [Command("flip"), Description("Flips a coin."), Category(Category.Fun)]
+        public async Task FlipCommand(CommandContext ctx) {
+            string coin = Random.Next(2) == 0 ? "HEADS" : "TAILS";
+            await ctx.RespondAsync($"{ctx.User.Username} flipped the coin and it landed on... ***{coin}!***");
+        }
+        
 
     }
 }
