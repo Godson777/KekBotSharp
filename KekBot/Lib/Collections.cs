bcb5ea434b4e30024c20ba4d1256c5ceadeed09b
tech6hutch <tech_hutch@verizon.net>
2020-03-19 17:50:37 -0400

    maybe fix

--- KekBot/Lib/Collections.cs
+++ KekBot/Lib/Collections.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using KekBot.Utils;
 
 namespace KekBot.Lib {
     internal class CommandInfoList : List<ICommandInfo> {
         public ICommandInfo GetFor(DSharpPlus.CommandsNext.Command cmd) =>
-            GetFor(cmd.Name);
+            GetFor(cmd.QualifiedName.Substring(0, cmd.QualifiedName.IndexOf(' ', System.StringComparison.Ordinal).FoundIndexOr(cmd.QualifiedName.Length)));
 
         public ICommandInfo GetFor(string name) =>
             this.First(cmdInfo => cmdInfo.Name.Equals(name, System.StringComparison.OrdinalIgnoreCase));
