b160056ff3aeaa1fb59b3016fd449df46de60f24
Godson <Godson777@users.noreply.github.com>
2020-03-18 22:32:37 -0400

    Revert "Fixed Ping Command (file soon to be renamed)"
    This reverts commit c97ef8033dd99b0e635c78c85be88637363ae776.

--- KekBot/Commands/TestCommand.cs
+++ KekBot/Commands/TestCommand.cs
@@ -3,13 +3,11 @@ using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using DSharpPlus.Interactivity;
 using ImageMagick;
-using ImageMagick.Defines;
 using KekBot.Menu;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +15,7 @@ namespace KekBot.Commands {
     public class TestCommand : BaseCommandModule {
         [Command("test"), Description("example test command")]
         public async Task Test(CommandContext ctx) {
-            //await ctx.RespondAsync(DiscordEmoji.FromGuildEmote(ctx.Client, 403114999578361856ul).GetDiscordName());
+            await ctx.RespondAsync(DiscordEmoji.FromGuildEmote(ctx.Client, 403114999578361856ul).GetDiscordName());
 
             /*var pag = new Paginator(ctx.Client.GetInteractivity());
             for (int i = 0; i < 20; i++) {
@@ -45,34 +43,16 @@ namespace KekBot.Commands {
 
             await pag.Display(ctx.Channel);*/
 
-            await ctx.TriggerTypingAsync();
+            /*await ctx.TriggerTypingAsync();
 
-            using (var client = new WebClient()) {
-                var _ = await client.OpenReadTaskAsync(new Uri(ctx.User.AvatarUrl));
-                var ava = new MagickImage(_);
-                using (var gif = new MagickImageCollection()) {
-                    var test = new MagickImage(MagickColor.FromRgba(255, 0, 0, 255), ava.Width, ava.Height);
-                    test.Format = MagickFormat.Gif;
-                    gif.Add(test.Clone());
-                    gif.Add(test.Clone());
-                    gif[0].Composite(ava, 0, 0, CompositeOperator.SrcOver);
-                    gif[0].AnimationDelay = 2;
-                    gif[1].Composite(ava, 50, 50, CompositeOperator.SrcOver);
-                    gif[1].AnimationDelay = 2;
-
-                    var settings = new QuantizeSettings {
-                        Colors = 256
-                    };
-                    gif.Quantize(settings);
-
-                    var stream = new MemoryStream(gif.ToByteArray());
-
-                    await ctx.RespondWithFileAsync("test.gif", stream, "test");
-                    stream.Dispose();
-                    ava.Dispose();
-                    test.Dispose();
-                }
-            }
+            WebClient client = new WebClient();
+            Stream stream = await client.OpenReadTaskAsync(new Uri(ctx.User.AvatarUrl));
+            IMagickImage ava = new MagickImage(stream);
+            Stream streamm = new MemoryStream();
+            IMagickImage test2 = new MagickImage(MagickColor.FromRgba(255, 0, 0, 255), ava.Width, ava.Height);
+            test2.Format = MagickFormat.Png64;
+            test2.Composite(ava, 50, 50, CompositeOperator.SrcOver);
+            await ctx.RespondWithFileAsync("test.png", new MemoryStream(test2.ToByteArray()), "test");*/
         }
 
     }
